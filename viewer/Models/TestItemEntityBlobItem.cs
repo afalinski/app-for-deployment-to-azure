628fb1887393f363f464d9a72d3bde35851ca371
Andrey Falinsky <afalinsky@memberplex.com>
2024-11-15 14:29:15 +0300

    Refactoring

---
+++ viewer/Models/TestItemEntityBlobItem.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace viewer.Models;
+
+public class TestItemEntityBlobItem
+{
+    [JsonProperty("Message")]
+    public string Message { get; set; }
+
+    [JsonProperty("CallbackUri")]
+    public string CallbackUri { get; set; }
+
+    [JsonProperty("IsSynced")]
+    public bool IsSynced { get; set; }
+
+    [JsonProperty("PartitionKey")]
+    public string PartitionKey { get; set; }
+
+    [JsonProperty("RowKey")]
+    public string RowKey { get; set; }
+
+    [JsonProperty("Timestamp")]
+    public DateTime Timestamp { get; set; }
+
+    [JsonProperty("ETag")]
+    public string ETag { get; set; }
+}
